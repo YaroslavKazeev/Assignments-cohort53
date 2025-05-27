22f030e7365956fd3e6fadecc1ed5c54180feaa8
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-05-27 21:10:10 +0200

    finished the JS assignment Week3

--- 1-JavaScript/Week3/assignment/ex2-mondaysWorth.test.js
+++ 1-JavaScript/Week3/assignment/ex2-mondaysWorth.test.js
@@ -1,14 +1,3 @@
-/*------------------------------------------------------------------------------
-Full description atL https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week4#exercise-2-whats-your-monday-worth
-
-- Complete the function names `computeEarnings`. It should take an array of
-  tasks and an hourly rate as arguments and return a formatted Euro amount
-  (e.g: `€11.34`) comprising the total earnings.
-- Use the `map` array function to take out the duration time for each task.
-- Multiply each duration by a hourly rate for billing and sum it all up.
-- Make sure the program can be used on any array of objects that contain a
-  `duration` property with a number value.
-------------------------------------------------------------------------------*/
 const mondayTasks = [
   {
     name: 'Daily standup',
@@ -30,16 +19,17 @@ const mondayTasks = [
 
 const hourlyRate = 25;
 
-function computeEarnings(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function computeEarnings(mondayTasks, hourlyRate) {
+  return `€${mondayTasks
+    .map((item) => (item['duration'] / 60) * hourlyRate)
+    .reduce((acc, elem) => acc + elem)
+    .toFixed(2)}`;
 }
 
 // ! Unit tests (using Jest)
 describe('js-wk3-mondaysWorth', () => {
   test('computeEarnings should take two parameters', () => {
-    // The `.length` property indicates the number of parameters expected by
-    // the function.
-    expect(computeEarnings).toHaveLength(2);
+    expect(computeEarnings.length).toBe(2);
   });
 
   test('computeEarnings should compute the earnings as a formatted Euro amount', () => {
--- 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
@@ -1,19 +1,3 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-JavaScript/Week4#exercise-3-lemon-allergy
-
-Your mom bought you a basket of fruit, because you're doing so well in
-HackYourFuture. How sweet of her!
-
-However, she forgot that you are allergic to lemons! Let's quickly dispose of
-them before you get an attack.
-
-Complete the function called `sanitizeFruitBasket`:
-
-- It should take two parameters: an array of strings representing a fruit basket 
-  to be sanitized and a string indicating the name of the fruit to be taken out.
-- Use the `filter` array method to take out the unwanted fruit.
-- Return a new array that contains the fruits without any lemons.
-------------------------------------------------------------------------------*/
 const fruitBasket = [
   'apple',
   'lemon',
@@ -24,27 +8,44 @@ const fruitBasket = [
   'lemon',
 ];
 
+const unwantedFruit = 'lemon';
+
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasket, unwantedFruit) {
+  return fruitBasket.filter((item) => item !== unwantedFruit);
 }
+
+module.exports = {
+  sanitizeFruitBasket,
+};
 
 // ! Unit tests (using Jest)
 describe('js-wk3-ex3-lemonAllergy', () => {
   test('sanitizeFruitBasket should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket.length).toBe(2);
   });
 
   test('sanitizeFruitBasket should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    sanitizeFruitBasket(originalFruitBasketContents, 'lemon');
+    expect(originalFruitBasketContents).toEqual(fruitBasket);
   });
 
   test('sanitizeFruitBasket should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(
+      sanitizeFruitBasket(
+        [
+          'apple',
+          'lemon',
+          'grapefruit',
+          'lemon',
+          'banana',
+          'watermelon',
+          'lemon',
+        ],
+        'lemon'
+      ).includes('lemon')
+    ).toBeFalsy();
   });
 });
