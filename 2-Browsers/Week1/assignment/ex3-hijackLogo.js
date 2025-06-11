f4d8bd5116e4f2197f05c54694c87e05a9e513c0
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-06-11 19:21:47 +0200

    Ex3 completed

--- 2-Browsers/Week1/assignment/ex3-hijackLogo.js
+++ 2-Browsers/Week1/assignment/ex3-hijackLogo.js
@@ -7,7 +7,14 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
    HackYourFuture logo instead.
 ------------------------------------------------------------------------------*/
 function hijackGoogleLogo() {
-  // TODO your code goes in here
+  logoDiv = document.getElementsByClassName('k1zIA rSk4se')[0];
+  svg = logoDiv.getElementsByTagName('svg')[0];
+  img = document.createElement('img');
+  img.width = svg.getAttribute('width');
+  img.height = svg.getAttribute('height');
+  img.src =
+    'https://raw.githubusercontent.com/HackYourFuture/Assignments/refs/heads/main/assets/hyf-logo-black-bg-small.png';
+  logoDiv.appendChild(img);
+  svg.remove();
 }
-
 hijackGoogleLogo();
